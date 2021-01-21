3d7c459c1c33a832e699f9483986cc404fb26fdd
awsandy <awsandy@amazon.co.uk>
2021-01-21 18:43:14 +0000

    up

---
+++ IRSA/data-irsa-trust-policy.tf
@@ -0,0 +1,18 @@
+### Enabling IAM Roles for Service Accounts
+data "aws_iam_policy_document" "cluster_trust_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.cluster.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.cluster.arn]
+      type        = "Federated"
+    }
+  }
+}
--- IRSA/serv-account.tf
+++ IRSA/serv-account.tf
@@ -3,7 +3,7 @@ automount_service_account_token = false
 
 metadata {
 annotations = {
-"eks.amazonaws.com/role-arn" = "arn:aws:iam::566972129213:role/eksctl-appmesh1-addon-iamserviceaccount-defa-Role1-I2LZV4H1I1Z3"
+"eks.amazonaws.com/role-arn" = iam_role.irsa-s3-test.arn
 }
 name = "iam-test"
 namespace = "default"
