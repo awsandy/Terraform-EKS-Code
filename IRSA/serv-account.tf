54422ab8fa36b16a95bb26b607365f7286b3d967
awsandy <awsandy@amazon.co.uk>
2021-01-21 18:58:31 +0000

    up

--- IRSA/serv-account.tf
+++ IRSA/serv-account.tf
@@ -3,7 +3,7 @@ automount_service_account_token = false
 
 metadata {
 annotations = {
-"eks.amazonaws.com/role-arn" = iam_role.irsa-s3-test.arn
+"eks.amazonaws.com/role-arn" = aws_iam_role.irsa-s3-test.arn
 }
 name = "iam-test"
 namespace = "default"
