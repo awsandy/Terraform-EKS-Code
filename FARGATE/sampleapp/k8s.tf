9dcab560b478dd712bf373cb70a38e027f69ede2
awsandy <awsandy@amazon.co.uk>
2021-01-23 15:17:23 +0000

    up

--- FARGATE/sampleapp/k8s.tf
+++ FARGATE/sampleapp/k8s.tf
@@ -1,11 +1,3 @@
-terraform {
-  required_providers {
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.0.1"
-    }
-  }
-}
 
 provider "kubernetes" {
   # Configuration options
