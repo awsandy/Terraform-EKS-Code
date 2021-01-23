2e58a877759edf66b912849dabc34c2572b44b2f
awsandy <awsandy@amazon.co.uk>
2021-01-23 15:15:55 +0000

    up

--- FARGATE/sampleapp/aws.tf
+++ FARGATE/sampleapp/aws.tf
@@ -14,10 +14,7 @@ terraform {
     source = "hashicorp/external"
     version = "~> 2.0"
     }
-    kubernetes = {
-    source = "hashicorp/kubernetes"
-    version = "~> 1.13.3"
-    }
+
     
   }
 }
@@ -29,4 +26,4 @@ provider "aws" {
 }
 provider "null" {}
 provider "external" {}
-provider "kubernetes" {}
+
