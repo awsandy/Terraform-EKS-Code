62c46a47ab9850e4e17fe63b261a0b5d73324a96
awsandy <awsandy@amazon.co.uk>
2021-01-23 14:06:42 +0000

    up

--- FARGATE/rand.tf
+++ FARGATE/rand.tf
@@ -3,11 +3,11 @@ resource "random_id" "id1" {
 }
 
 
-resource "random_id" "id2" {
-  keepers = {
-    # Generate a new id each time we switch to a new AMI id
-    name = "${var.ami_id}"
-  }
+#resource "random_id" "id2" {
+#  keepers = {
+#    # Generate a new id each time we switch to a new AMI id
+#    name = "${var.ami_id}"
+#  }
 
-  byte_length = 8
-}
+#  byte_length = 8
+#}
