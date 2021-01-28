606bf00dba24513e78bf70a310d9bcffa369e2d7
awsandy <awsandy@amazon.co.uk>
2021-01-28 19:56:56 +0000

    up

--- SELF/get-bucket-name.sh
+++ SELF/get-bucket-name.sh
@@ -5,28 +5,8 @@
 idfile=$HOME/.tfid
 gotid=0
 rm -f tf-out.txt
-(terraform state list 2> /dev/null) > /dev/null
-if [ $? -eq 0 ]; then
-    echo "it might exist" >> tf-out.txt
-    terraform state list | grep random_id.id1 > /dev/null
-    if [ $? -eq 0 ]; then
-        echo "it does get it" >> tf-out.txt
-        id=$(terraform state show random_id.id1 | grep hex | cut -f2 -d'=' | tr -d '"' | tr -d ' ')
-        echo "id from state = $id" >> tf-out.txt
-        gotid=1
-        echo "check $idfile exists" >> tf-out.txt
-        if [ -f "$idfile" ]; then
-                echo "$idfile exists" >> tf-out.txt
-        else
-            echo "$idfile does not exist - write it" >> tf-out.txt
-            printf "{\n" > $idfile
-            printf "\"id\" : \"%s\"\n" $id >> $idfile
-            printf "}\n" >> $idfile
-            cat $idfile >> tf-out.txt
-        fi
 
-    fi
-fi
+# Method 1 - it's in the hidden file already 
 if [ $gotid -eq 0 ]; then
     # state does not exist
     echo "no terraform random_id.id1 exists" >> tf-out.txt
@@ -37,13 +17,15 @@ if [ $gotid -eq 0 ]; then
             echo "id from file = $id" >> tf-out.txt
             idl=$(echo $id | wc -c)
             if [ $idl -lt 16 ]; then
-                exit 2
+                exit 2 # id should be 8 byte hexidecimal value
             else
                 gotid=1
             fi
         fi
 fi
 
+# Method 2 - a single state file bucket exists
+# If thefre are multiple we will exit with an error - as that is unexpected
 if [ $gotid -eq 0 ]; then       
             echo "no random_id and no $idfile" >> tf-out.txt
             echo "look for bucket" >> tf-out.txt
@@ -75,6 +57,31 @@ if [ $gotid -eq 0 ]; then
             fi
 fi
 
+# Method 3 - it's in the state already - most likely we're setting up depends_on ramdom_id
+if [ $gotid -eq 0 ]; then
+    (terraform state list 2> /dev/null) > /dev/null
+    if [ $? -eq 0 ]; then
+        echo "it might exist" >> tf-out.txt
+        terraform state list | grep random_id.id1 > /dev/null
+        if [ $? -eq 0 ]; then
+            echo "it does get it" >> tf-out.txt
+            id=$(terraform state show random_id.id1 | grep hex | cut -f2 -d'=' | tr -d '"' | tr -d ' ')
+            echo "id from state = $id" >> tf-out.txt
+            gotid=1
+            echo "check $idfile exists" >> tf-out.txt
+            if [ -f "$idfile" ]; then
+                    echo "$idfile exists" >> tf-out.txt
+            else
+                echo "$idfile does not exist - write it" >> tf-out.txt
+                printf "{\n" > $idfile
+                printf "\"id\" : \"%s\"\n" $id >> $idfile
+                printf "}\n" >> $idfile
+                cat $idfile >> tf-out.txt
+            fi
+
+        fi
+    fi
+fi
 
 BUCKET_NAME=`printf "tf-eks-state-%s" $id | awk '{print tolower($0)}'`
 jq -n --arg bn "$BUCKET_NAME" '{"Name":$bn}'
