0a79c14e32571ec2ec7ebb4b9f71d368878080a3
awsandy <awsandy@amazon.co.uk>
2021-01-30 14:15:17 +0000

    up

--- SELF/child/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-backend "s3" {
-bucket="tf-eks-state-45fe452a933df254"
-key = "terraform/terraform_state_child.tfstate"
-region = "eu-west-1"
-dynamodb_table = "tf_lock_45fe452a933df254_child"
-encrypt = true
-}
-}
---
+++ SELF/child/delete.sh
@@ -0,0 +1,5 @@
+rm -f backend.tf .tfph1
+echo "Init away from s3"
+terraform init -no-color -force-copy
+echo "Destroy"
+terraform destroy -no-color -auto-approve
