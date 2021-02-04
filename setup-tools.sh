60670d8dfbfbaf278ab65bb1fca6f5c3425b5607
awsandy <awsandy@amazon.co.uk>
2021-02-04 12:52:34 +0000

    up

--- setup-tools.sh
+++ setup-tools.sh
@@ -27,10 +27,13 @@ aws sts get-caller-identity --query Arn | grep eksworkshop-admin > /dev/null
 if [ $? -eq 0 ]; then
   rm -vf ${HOME}/.aws/credentials
   export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+  export TF_VAR_region=$(echo $AWS_REGION)
   export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
   test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set !!
   echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
   echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+  export TF_VAR_region=${AWS_REGION}
+  echo "export TF_VAR_region=${AWS_REGION}" | tee -a ~/.bash_profile
   aws configure set default.region ${AWS_REGION}
   aws configure get region
 fi
