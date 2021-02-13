8eb10767545908c501464f4eadd2477111faf4ec
awsandy <awsandy@amazon.co.uk>
2021-02-13 19:52:30 +0000

    up

--- Launch/lb2/null_post_policy.tf
+++ Launch/lb2/null_post_policy.tf
@@ -13,9 +13,9 @@ resource "null_resource" "post-policy" {
         cn=$(echo ${data.aws_eks_cluster.eks_cluster.name})
         echo "$reg $cn $acc"
         ./post-policy.sh $reg $cn $acc
-        echo "reannotate nodes"
-        cd ../eks-cidr
-        ./reannotate-nodes.sh $cn
+        #echo "reannotate nodes"
+        #cd ../eks-cidr
+        #./reannotate-nodes.sh $cn
         echo "done"
      EOT
   }
