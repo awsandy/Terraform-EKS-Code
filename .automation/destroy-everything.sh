20a1a5b6108240fa3f02d1aeecb93b798862fdc3
awsandy <awsandy@amazon.co.uk>
2021-02-14 18:20:13 +0000

    up

--- .automation/destroy-everything.sh
+++ .automation/destroy-everything.sh
@@ -2,7 +2,7 @@ date
 echo "Pre cli based actions ..."
 userid=$(aws iam list-service-specific-credentials --user-name git-user | jq -r .ServiceSpecificCredentials[0].ServiceSpecificCredentialId)
 if [ "$userid" != "null" ]; then
-echo "destroying git user credentaisl for $userid"
+echo "destroying git user credentails for $userid"
 aws iam delete-service-specific-credential --service-specific-credential-id $userid --user-name git-user
 fi
 # Empty codepipeline bucket ready for delete
