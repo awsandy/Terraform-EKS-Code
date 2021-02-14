611a56d50fe1221863c1b5a80b578577371a31c1
awsandy <awsandy@amazon.co.uk>
2021-02-14 20:40:55 +0000

    tool update

--- setup-tools.sh
+++ setup-tools.sh
@@ -41,10 +41,10 @@ fi
 
 if [ ! `which terraform 2> /dev/null` ]; then
   echo "Install Terraform"
-  wget https://releases.hashicorp.com/terraform/0.14.5/terraform_0.14.5_linux_amd64.zip
-  unzip -qq terraform_0.14.5_linux_amd64.zip
+  wget https://releases.hashicorp.com/terraform/0.14.6/terraform_0.14.6_linux_amd64.zip
+  unzip -qq terraform_0.14.6_linux_amd64.zip
   sudo mv terraform /usr/local/bin/
-  rm -f terraform_0.14.5_linux_amd64.zip
+  rm -f terraform_0.14.6_linux_amd64.zip
 fi
 
 if [ ! -f $HOME/.terraform.d/plugin-cache ];then
