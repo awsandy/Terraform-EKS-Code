6b04a60accae53832fab8eab7143e00e3119bb15
awsandy <awsandy@amazon.co.uk>
2021-03-16 20:58:23 +0000

    up

--- .automation/test.sh
+++ .automation/test.sh
@@ -8,7 +8,7 @@ rc=$(kubectl get pods -A | grep Running | wc -l)
 if [ $rc -lt 23 ]; then 
 echo "ERROR: Found only $rc pods running - expected 23"
 else
-echo "Passed running pod count"
+echo "PASSED: running pod count $rc pods"
 fi
 
 rc=$(kubectl get pods -A -o wide | grep 100.64 | wc -l)
@@ -46,7 +46,7 @@ echo "ERROR: Found only $rc fargate pods running - expected 2"
 else
 echo "PASSED: fargate pod count"
 fi
-helm ls -A | grep aws-load-balancer-controller | grep deployed && echo "PASSED: helm lb deployed" || echo "FAILED: helm lb deployed"
-kubectl get pods -n kube-system | grep aws-load-balancer-controller | grep Running && echo "PASSED: lb pod" || echo "FAILED: lb pod"
-kubectl logs jobs/eks-iam-test-s3 | grep tf && echo "PASSED: irsa test" || echo "FAILED: irsa test"
+helm ls -A | grep aws-load-balancer-controller | grep deployed > /dev/null && echo "PASSED: helm lb deployed" || echo "FAILED: helm lb deployed"
+kubectl get pods -n kube-system | grep aws-load-balancer-controller | grep Running > /dev/null && echo "PASSED: lb pod" || echo "FAILED: lb pod"
+kubectl logs jobs/eks-iam-test-s3 | grep tf > /dev/null && echo "PASSED: irsa test" || echo "FAILED: irsa test"
 
