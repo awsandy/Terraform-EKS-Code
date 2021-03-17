c5bccd6776f6b86a0b6ad780f097966b48306f06
awsandy <awsandy@amazon.co.uk>
2021-03-17 21:19:34 +0000

    up

--- extra/nodeg2/launch_template.tf
+++ extra/nodeg2/launch_template.tf
@@ -5,7 +5,7 @@ resource "aws_launch_template" "lt-ng2" {
   tags                   = {}
   image_id               = data.aws_ssm_parameter.eksami.value
   #user_data              = base64encode(local.eks-node-private-userdata)
-  user_data = cloudinit_config.example.rendered
+  user_data = data.cloudinit_config.example.rendered
   vpc_security_group_ids = [data.terraform_remote_state.net.outputs.allnodes-sg]
   tag_specifications {
     resource_type = "instance"
