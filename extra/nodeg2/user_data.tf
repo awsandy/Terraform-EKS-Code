98b459f8409418cc35eca895d27efc4153f99d80
awsandy <awsandy@amazon.co.uk>
2021-03-17 18:47:01 +0000

    up

--- extra/nodeg2/user_data.tf
+++ extra/nodeg2/user_data.tf
@@ -9,9 +9,9 @@ Content-Type: text/x-shellscript; charset="us-ascii"
 #!/bin/bash -xe
 sudo /etc/eks/bootstrap.sh --apiserver-endpoint '${data.aws_eks_cluster.eks_cluster.endpoint}' --b64-cluster-ca '${data.aws_eks_cluster.eks_cluster.certificate_authority[0].data}' '${data.aws_eks_cluster.eks_cluster.name}'
 echo "Running custom user data script" > /tmp/me.txt
-yum install -y amazon-ssm-agent awslogs amazon-cloudwatch-agent
+yum install -y amazon-ssm-agent awslogs amazon-cloudwatch-agent iptables-services
 echo "yum'd agent" >> /tmp/me.txt
-systemctl enable amazon-ssm-agent && systemctl start amazon-ssm-agent iptables-services
+systemctl enable amazon-ssm-agent && systemctl start amazon-ssm-agent 
 # disable meta data
 #iptables --insert FORWARD 1 --in-interface eni+ --destination 169.254.169.254/32 --jump DROP
 #systemctl enable --now iptables
