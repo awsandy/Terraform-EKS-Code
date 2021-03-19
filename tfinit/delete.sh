c08c1ab9429a6f23e04d00a547b0acccb0b07d58
awsandy <awsandy@amazon.co.uk>
2021-03-19 17:07:34 +0000

    tfinint clean

--- tfinit/delete.sh
+++
@@ -1,5 +0,0 @@
-rm -f backend.tf .tfph1
-echo "Init away from s3"
-terraform init -no-color -force-copy
-echo "Destroy"
-terraform destroy -no-color -auto-approve -lock=false
--- tfinit/shift-state.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Exit if any of the intermediate steps fail
-#set -e
-# get id
-
-
-# write backend.tf
-
-
