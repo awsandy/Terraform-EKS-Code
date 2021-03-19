47321c7f8d6fe89afbdbc3811d952936ccc5465a
awsandy <awsandy@amazon.co.uk>
2021-03-19 17:46:32 +0000

    up

--- tfinit/null_idfile.tf
+++ tfinit/null_idfile.tf
@@ -7,14 +7,20 @@ resource "null_resource" "gen_idfile" {
     when    = create
     command = <<EOT
         noout=${var.no-output}
-        idfile=$HOME/.tfid
-	    rm -f $idfile
+        #idfile=$HOME/.tfid
+	      #rm -f $idfile
         id=${random_id.id1.hex}
         #echo $id
-        printf "{\n" > $idfile
-        printf "\"id\" : \"%s\"\n" $id >> $idfile
-        printf "}\n" >> $idfile
+        #printf "{\n" > $idfile
+        #printf "\"id\" : \"%s\"\n" $id >> $idfile
+        #printf "}\n" >> $idfile
         #echo "done"
+        varfile="../.stub/var-tfid.tf"
+        printf "variable \"tfid\" {\n" > $varfile
+        printf "description = "The unique ID for the project"\n" >> $varfile
+        printf "type        = string\n" >> $varfile
+        printf "default     = \"%s\"\n" $id >> $varfile
+        printf "}\n" >> $varfile
 
      EOT
 
