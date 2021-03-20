89cd56db199d9ce10068ad95147155d77113610c
awsandy <awsandy@amazon.co.uk>
2021-03-20 10:59:44 +0000

    up

--- .stub/remote-net.tf
+++ .stub/remote-net.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "net" {
 
   backend = "s3"
   config = {
-    bucket = format("tf-eks-state-%s", data.external.tfid.result.Name)
+    bucket = format("tf-eks-state-%s", var.tfid)
     region = data.aws_region.current.name
     key    = "terraform/tf_state_net.tfstate"
   }
