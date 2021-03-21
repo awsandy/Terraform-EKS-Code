c24b15f7973c7a5030bc7150de394d4ddbb0ede8
awsandy <awsandy@amazon.co.uk>
2021-03-21 19:25:32 +0000

    up

--- .automation/build-all.sh
+++ .automation/build-all.sh
@@ -3,7 +3,7 @@ rm -f build.log
 date >> build.log
 cur=`pwd`
 buildok=1
-dirs="tfinit Launch/net Launch/iam Launch/c9net Launch/cluster Launch/nodeg Launch/lb2  Beginner/eks-cidr Launch/cicd Intermediate/sampleapp extra/nodeg2 extra/eks-cidr2 extra/sampleapp2 Beginner/fargate Beginner/fargate/fargateapp Beginner/irsa Advanced/app-mesh"
+dirs="tfinit Launch/net Launch/iam Launch/c9net Launch/cluster Launch/nodeg Launch/lb2 Launch/cicd Intermediate/sampleapp Beginner/fargate Beginner/fargate/fargateapp Beginner/irsa extra/nodeg2 extra/eks-cidr2 extra/sampleapp2 Advanced/app-mesh"
 for i in `echo $dirs`;do
     echo $i
     ./build-stage.sh $i 2>&1 | tee -a build.log
