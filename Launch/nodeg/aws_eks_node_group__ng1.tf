c335479bd1566ab814bec05830d9ac9778bbaa91
awsandy <awsandy@amazon.co.uk>
2021-06-13 21:29:46 +0100

    up

--- Launch/nodeg/aws_eks_node_group__ng1.tf
+++ Launch/nodeg/aws_eks_node_group__ng1.tf
@@ -30,8 +30,8 @@ resource "aws_eks_node_group" "ng1" {
   }
 
   scaling_config {
-    desired_size = 2
-    max_size     = 3
+    desired_size = 3
+    max_size     = 6
     min_size     = 1
   }
 
