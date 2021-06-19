d9a6ef446d96ee71e495d7454fe7d312f15bc714
awsandy <awsandy@amazon.co.uk>
2021-06-19 17:26:29 +0100

    up

--- setup-tools.sh
+++ setup-tools.sh
@@ -77,14 +77,9 @@ if [ ! -f ~/.ssh/id_rsa ]; then
   ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
   chmod 600 ~/.ssh/id*
 fi
+# cleanup key_pair if already there
 aws ec2 delete-key-pair --key-name "eksworkshop" > /dev/null
-aws ec2 import-key-pair --key-name "eksworkshop" --public-key-material fileb://~/.ssh/id_rsa.pub > /dev/null
-echo "KMS key"
-aws kms create-alias --alias-name alias/eksworkshop --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
-export TF_VAR_MASTER_ARN=$(aws kms describe-key --key-id alias/eksworkshop --query KeyMetadata.Arn --output text)
-if [ ! -z $TF_VAR_MASTER_ARN ];then
-echo "export TF_VAR_MASTER_ARN=${TF_VAR_MASTER_ARN}" | tee -a ~/.bash_profile
-fi
+
 
 echo "git-remote-codecommit"
 pip install git-remote-codecommit > /dev/null
