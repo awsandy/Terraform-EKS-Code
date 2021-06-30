0b38495223c5c59f6001b29d9e994789a166b242
awsandy <awsandy@amazon.co.uk>
2021-06-30 17:42:11 +0100

    up

--- Advanced/.bottlerocket/data-subnet-i.tf
+++ Advanced/.bottlerocket/data-subnet-i.tf
@@ -1,9 +1,9 @@
 data "aws_subnet" "i1" {
-  id = data.terraform_remote_state.net.outputs.isolated_subnet_id[2]
+  id = data.terraform_remote_state.net.outputs.isolated_subnet_id[0]
 }
 
 data "aws_subnet" "i2" {
-  id = data.terraform_remote_state.net.outputs.isolated_subnet_id[2]
+  id = data.terraform_remote_state.net.outputs.isolated_subnet_id[1]
 
 }
 
