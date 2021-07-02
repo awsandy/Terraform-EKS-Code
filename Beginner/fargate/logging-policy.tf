31ee47db559832501c95d2c61cd27593b709c0ee
awsandy <awsandy@amazon.co.uk>
2021-07-02 21:27:00 +0100

    up

--- Beginner/fargate/logging-policy.tf
+++ Beginner/fargate/logging-policy.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "eks-fargate-logging-policy" {
-  name        = "eks-fargate-logging-policy"
+  name        = format("eks-fargate-log-policy-%s",var.tfid)
   path        = "/"
   description = "eks-fargate-logging-policy"
 
