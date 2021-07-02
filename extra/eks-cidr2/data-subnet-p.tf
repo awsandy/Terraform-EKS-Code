c2b4925729d1f2eb4ac8a0d190c95953f6796e17
awsandy <awsandy@amazon.co.uk>
2021-07-02 21:48:32 +0100

    up

--- extra/eks-cidr2/data-subnet-p.tf
+++ extra/eks-cidr2/data-subnet-p.tf
@@ -1,26 +1,15 @@
 data "aws_subnet" "p1" {
-  vpc_id = data.terraform_remote_state.net.outputs.eks-vpc
-  filter {
-    name   = "tag:workshop"
-    values = ["subnet-p1"]
-  }
+  id = data.terraform_remote_state.net.outputs.private_subnet_id[0]
+
 }
 
 data "aws_subnet" "p2" {
-  vpc_id = data.terraform_remote_state.net.outputs.eks-vpc
+  id = data.terraform_remote_state.net.outputs.private_subnet_id[1]
 
-  filter {
-    name   = "tag:workshop"
-    values = ["subnet-p2"]
-  }
 }
 
 
 data "aws_subnet" "p3" {
-  vpc_id = data.terraform_remote_state.net.outputs.eks-vpc
+  id = data.terraform_remote_state.net.outputs.private_subnet_id[2]
 
-  filter {
-    name   = "tag:workshop"
-    values = ["subnet-p3"]
-  }
 }
