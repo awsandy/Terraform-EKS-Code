dbd3e74895f597aea33fa6b918910e08aad6e1d1
awsandy <awsandy@amazon.co.uk>
2021-07-03 13:55:30 +0100

    up

---
+++ .automation/build-infra.sh
@@ -0,0 +1,22 @@
+echo "circa 45 minutes ..."
+rm -f build.log
+date >> build.log
+cur=`pwd`
+cd ~/environment/tfekscode/Launch/lb2
+curl -o iam-policy.json https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/main/docs/install/iam_policy.json -s
+cd $cur
+buildok=1
+#dirs="tfinit Launch/net Launch/iam Launch/c9net Launch/cluster Launch/nodeg Launch/lb2 Launch/cicd  Beginner/fargate Intermediate/Bottlerocket extra/nodeg2 extra/eks-cidr2"
+dirs="tfinit Launch/net Launch/iam Launch/c9net Launch/cluster Launch/nodeg Launch/lb2 Launch/cicd  Beginner/fargate Intermediate/Bottlerocket"
+
+for i in `echo $dirs`;do
+    echo $i
+    ./build-stage.sh $i 2>&1 | tee -a build.log
+    grep Error: build.log
+    if [[ $? -eq 0 ]];then
+        echo "Error: in build.log"
+        exit
+    fi
+done
+date >> build.log
+
