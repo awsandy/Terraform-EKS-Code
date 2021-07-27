3e2e6756baebc2a900f4da2387eb4fce1f05dc2c
awsandy <awsandy@amazon.co.uk>
2021-07-27 08:46:03 +0100

    up

--- Intermediate/Bottlerocket/br-admin.sh
+++ Intermediate/Bottlerocket/br-admin.sh
@@ -5,13 +5,16 @@ qid=`aws ssm send-command --instance-ids $INSTID \
     --parameters commands="apiclient -u /settings" \
     --output text \
     --query "Command.CommandId"`
+echo $qid
 aws ssm list-command-invocations --command-id $qid --details --query 'CommandInvocations[*].CommandPlugins[*][Output]'
+echo "enable admin"
 qid=`aws ssm send-command --instance-ids $INSTID \
 --document-name "AWS-RunShellScript" \
 --comment "Bottlerocket API Enable Admin" \
 --cli-input-json file://enable-admin.json
 --output text \
 --query "Command.CommandId"`
+echo $qid
 aws ssm list-command-invocations --command-id $qid \
     --details \
     --query 'CommandInvocations[*].CommandPlugins[*][Output]'
