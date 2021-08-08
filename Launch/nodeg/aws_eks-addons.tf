8b9bc95c3853e15a64f5e437788a275038d58966
awsandy <awsandy@amazon.co.uk>
2021-08-08 11:15:13 +0100

    up

--- Launch/nodeg/aws_eks-addons.tf
+++ Launch/nodeg/aws_eks-addons.tf
@@ -3,7 +3,7 @@ resource "aws_eks_addon" "vpc-cni" {
   depends_on     = [aws_eks_node_group.ng1]
   cluster_name = data.aws_eks_cluster.eks_cluster.name
   addon_name   = "vpc-cni"
-  addon_version = "1.9.0"
+  addon_version = "v1.9.0-eksbuild.1"
 }
 
 resource "aws_eks_addon" "kube-proxy" {
