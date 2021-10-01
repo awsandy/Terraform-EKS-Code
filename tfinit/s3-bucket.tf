8b8eadaca62685743dc807e81fa1ef5302ab110f
awsandy <awsandy@amazon.co.uk>
2021-10-01 12:30:15 +0100

    up

--- tfinit/s3-bucket.tf
+++ tfinit/s3-bucket.tf
@@ -31,5 +31,6 @@ resource "aws_s3_bucket_public_access_block" "example" {
 	bucket = aws_s3_bucket.terraform_state.id
 	block_public_acls   = true
 	block_public_policy = true
+  ignore_public_acls = true
   restrict_public_buckets = true
 }
