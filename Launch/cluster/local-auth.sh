16aaeaa9155f591c05b33cd29cfc46501c801a96
awsandy <awsandy@amazon.co.uk>
2021-12-08 21:26:18 +0000

    up

---
+++ Launch/cluster/local-auth.sh
@@ -0,0 +1,13 @@
+echo "local auth"
+c9builder=$(aws cloud9 describe-environment-memberships --environment-id=$C9_PID | jq -r '.memberships[].userArn')
+if echo ${c9builder} | grep -q user; then
+	rolearn=${c9builder}
+        echo Role ARN: ${rolearn}
+elif echo ${c9builder} | grep -q assumed-role; then
+        assumedrolename=$(echo ${c9builder} | awk -F/ '{print $(NF-1)}')
+        rolearn=$(aws iam get-role --role-name ${assumedrolename} --query Role.Arn --output text) 
+        echo Role ARN: ${rolearn}
+fi
+## need to Terraform this
+eksctl create iamidentitymapping --cluster eksworkshop-eksctl --arn ${rolearn} --group system:masters --username admin
+kubectl describe configmap -n kube-system aws-auth
