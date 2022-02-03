5627dc8518d1667fe5f9d65687145b04aa0b7c12
awsandy <awsandy@amazon.co.uk>
2022-02-03 15:26:32 +0000

    up

--- .stub/base.tf
+++ .stub/base.tf
@@ -19,6 +19,16 @@ terraform {
       version = "= 2.7.1"
     }
 
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.4.1"
+    }
+
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1.0"
+    }
+
   }
 }
 
