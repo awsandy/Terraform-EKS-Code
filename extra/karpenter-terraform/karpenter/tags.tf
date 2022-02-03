23daf2278708bed180beaa0d40e3d65276de6fd3
awsandy <awsandy@amazon.co.uk>
2022-02-03 17:21:36 +0000

    up

--- extra/karpenter-terraform/karpenter/tags.tf
+++ extra/karpenter-terraform/karpenter/tags.tf
@@ -4,6 +4,6 @@
 resource "aws_ec2_tag" "karpenter_tags" {
   for_each    = data.aws_eks_cluster.eks.vpc_config.0.subnet_ids
   resource_id = each.value
-  key         = format("kubernetes.io/cluster/%s", var.cluster_name)
+  key         = format("kubernetes.io/cluster/%s", var.cluster-name)
   value       = "true"
 }
